a119a493262572eaac4f23a7b87a8e3b014885c9
miquelalcon <miquel.1996@gmail.com>
2019-03-27 20:09:42 +0100

    ADM

--- wtf/src/method_tester.py
+++ wtf/src/method_tester.py
@@ -7,6 +7,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
 #from sklearn.feature_selection import RFE
 
 
@@ -19,7 +20,7 @@ methods = {
     'NB':   GaussianNB(),
     'NN':   KNeighborsClassifier(n_neighbors=28),
     'DT':   tree.DecisionTreeClassifier(),
-    'SVM':  SVC(kernel="linear", C=1)
+    'SVM':  SVC(kernel="poly", C=10, gamma='auto')
 }
 
 def preprocess(data,save=False):
@@ -36,36 +37,31 @@ def predict(method,X_train,y_train,X_test,y_test):
     y_pred = method.fit(X_train,y_train).predict(X_test)
     return accuracy_score(y_test,y_pred, normalize=True), y_pred
 
-if __name__ == '__main__':
+def predict_with_model(model,X_test,y_test):
+    y_pred = model.predict(X_test)
+    return accuracy_score(y_test,y_pred, normalize=True), y_pred
 
-    data = pd.read_csv('./data/processed.csv')
-
-    if need_preprocess:
-        data = preprocess(data,True)
-
-    X = data.drop(['num'], axis=1)
-    y = data.num
-
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-
-    #score,_ = predict(X_train,y_train,X_test,y_test)
-    #print(score)
-
-
-    method = methods['NN']
+def perform_experiment(method_name,X,y):
+    method = methods[method_name]
+    print('================== %s =================='%method_name)
     scores = cross_val_score(method, X, y, cv=10)
     main_score = np.mean(scores)
-    print('Without feature elimination: %0.5f'%main_score)
+    print('Cross validation without feature elimination: %0.5f'%main_score)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    score,_ = predict(method,X_train,y_train,X_test,y_test)
+    print('Train-test without feature elimination: %0.5f'%score)
+
 
     X_aux = X.copy()
     features_to_eliminate = []
+    print('Dropping features:')
     for feature in list(X):
         #print('Drop %s feature.'%feature)
         X_aux = X.drop(feature, axis=1)
         scores = cross_val_score(method, X_aux, y, cv=10)
         score = np.mean(scores)
-        print('Drop %s feature: %0.5f.'%(feature,score))
+        print('  - Drop %s feature: %0.5f.'%(feature,score))
         if score > main_score:
             features_to_eliminate.append(feature)
     if not features_to_eliminate:
@@ -78,14 +74,63 @@ if __name__ == '__main__':
         # score,_ = predict(method,X_train,y_train,X_test,y_test)
         scores = cross_val_score(method, X, y, cv=10)
         score = np.mean(scores)
-        print(score)
+        print('Cross validation: %0.5f'%score)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    
+    binary_y_train = y_train.copy()
+    binary_y_test = y_test.copy()
 
-    score,_ = predict(method,X_train,y_train,X_test,y_test)
-    print('Final predictor accuracy: %0.5f'%score)
+    binary_y_train.num = y_train.apply(lambda x: int(x > 0))
+    binary_y_test.num = y_test.apply(lambda x: int(x > 0))
 
 
 
-    # print('Number of mislabeled points out of a total %d points : %d'
-    #     % (data.shape[0],(data[-1] != pred).sum()))
+    score,binary_y_pred = predict(method,X_train,binary_y_train,X_test,binary_y_test)
+    print('Final predictor (train-test) accuracy: %0.5f'%score)
+    print('Confusion matrix:')
+    cm = confusion_matrix(y_test,binary_y_pred)
+    print(cm)
+    position_saver = []
+    new_X_test = []
+    new_y_test = []
+    for i,v in enumerate(binary_y_pred):
+        position_saver.append(i)
+        new_y_test.append(v)
+        new_X_test.append(X_test[i])
+    score,y_pred = predict(method,X_train,y_train,new_X_test,new_y_test)
+    print('Final predictor (train-test) accuracy: %0.5f'%score)
+    print('Confusion matrix:')
+    cm = confusion_matrix(y_test,y_pred)
+
+    
+
+
+
+if __name__ == '__main__':
+
+    data = pd.read_csv('./data/processed.csv')
+
+    if need_preprocess:
+        data = preprocess(data,True)
+
+    X = data.drop(['num'], axis=1)
+    y = data.num
+    #y = y.apply(lambda x: int(x > 0))
+
+
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+
+    #score,_ = predict(X_train,y_train,X_test,y_test)
+    #print(score)
+
+
+    #for method_name,method in methods.items():
+    perform_experiment('NB',X,y)
+    
+    
+    # X = data[data['num'] != 0]
+    # y = X.num
+    # X = X.drop(['num'], axis=1)
+    # perform_experiment('NB',X,y)
