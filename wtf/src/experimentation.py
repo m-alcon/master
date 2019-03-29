9b0f2fa2a6cce3134120c4a02a77bf8ec8f438fc
miquelalcon <miquel.1996@gmail.com>
2019-03-29 16:19:43 +0100

    ADM Deliverable 1 final

--- wtf/src/experimentation.py
+++ wtf/src/experimentation.py
@@ -8,7 +8,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix
-#from sklearn.feature_selection import RFE
 
 
 header = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach',
