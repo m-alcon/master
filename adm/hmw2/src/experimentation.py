53296fa812576ee5e8a43a8134a229dcae29969d
miquelalcon <miquel.1996@gmail.com>
2019-04-18 16:21:23 +0200

    adm hmw2 ADD fit, predict with EM + center gen.

--- adm/hmw2/src/experimentation.py
+++ adm/hmw2/src/experimentation.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix
+from sklearn import metrics
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
 from matplotlib import rcParams
@@ -24,9 +23,6 @@ header = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach',
 
 title_length = 70
 
-methods = {
-}
-
 def print_title(char,title):
     bar_size = int(np.ceil(float(title_length - len(title) - 2)/2))
     if bar_size < 1:
@@ -35,50 +31,76 @@ def print_title(char,title):
         bar = char*bar_size
         print('%s %s %s'%(bar,title,bar))
 
-def preprocess(data,save=False):
-    count_erased_rows = 0
-    for column in data:
-        count_erased_rows += sum(data[column].apply(str) == '?')
-        data = data[data[column].apply(str) != '?']
-    print('%d rows which contain missing values erased.'%count_erased_rows)
-    if save:
-        data.to_csv('data/processed.csv',index=False)
-    return data
+def generate_centers(n):
+    div = np.floor(np.sqrt(n))
+    x = int(np.floor(n/div) + n%div)
+    y = int(np.floor(n/div))
+    print(x,y)
+    centers = []
+    for j in range(y):
+        for i in range(x):
+            centers.append([i*2,j*2])
+    return centers
 
 
-def target_to_binary(y, y_train, y_test):
-    binary_y = y.copy()
-    binary_y_train = y_train.copy()
-    binary_y_test = y_test.copy()
-
-    binary_y = y.apply(lambda x: int(x > 0))
-    binary_y_train = y_train.apply(lambda x: int(x > 0))
-    binary_y_test = y_test.apply(lambda x: int(x > 0))
-    return binary_y, binary_y_train, binary_y_test
+def map_labels(truth, pred):
+    mapping = np.zeros((max(pred)+1,max(truth)+1))
+    for t,p in zip(truth,pred):
+        mapping[p][t] += 1
+    return [np.argmax(m) for m in mapping]
 
 
 
 if __name__ == '__main__':
-
-    centers = [[1, 1], [-1, -1], [-1, 1], [1, -1]]
     #n_centers = 8
     #centers = [[n_centers*x,n_centers*y] for x,y in np.random.rand(n_centers,2)]
     #centers = np.random.uniform(0,n_centers+4, (n_centers, 2))
     #print(centers)
 
-    cNorm  = colors.Normalize(vmin=0, vmax=len(centers))
-    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap='gist_rainbow')
+    # Centers of the clusters to generate
+    centers = generate_centers(16)
+    n_centers = 16
 
-    data, truth_labels = make_blobs(n_samples=300, centers=centers, cluster_std=0.4,
-                            random_state=73)
+    # Methods to compare
+    methods = {
+        'EM': GaussianMixture(n_components=n_centers, covariance_type='full')
+    }
 
-    colors = [scalarMap.to_rgba(l) for l in truth_labels]
-
+    # Generation of data
+    data, truth_labels = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4,
+                            random_state=55)
     x = [d[0] for d in data]
     y = [d[1] for d in data]
 
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    # Automatic cluster coloring
+    cNorm  = colors.Normalize(vmin=0, vmax=n_centers)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap='gist_rainbow')
+    truth_colors = [scalarMap.to_rgba(l) for l in truth_labels]
 
-    #fig, ax = plt.subplot()
-    plt.scatter(x,y,c=colors)
-    plt.show()
+    # Clustering
+    methods['EM'].fit(data, y=truth_labels)
+    pred_labels = methods['EM'].predict(data)
+    pred_colors = [scalarMap.to_rgba(l) for l in pred_labels]
+
+    # Map truth labels with predicted labels
+    label_map = map_labels(truth_labels,pred_labels)
+    mapped_labels = [label_map[l] for l in pred_labels]
+
+    # Prepare colors for the plot
+    mixed_colors = []
+    for t,p in zip(truth_labels,mapped_labels):
+        if t == p:
+            mixed_colors.append(scalarMap.to_rgba(t))
+        else:
+            mixed_colors.append('#000000') # Color of a bad prediction
+    
+    # Print score
+    print('Score:', metrics.adjusted_rand_score(truth_labels, pred_labels))
+    
+    # Generate and save plots
+    plt.scatter(x,y,c=truth_colors)
+    plt.savefig('./plots/truth.pdf')
+    plt.scatter(x,y,c=pred_colors)
+    plt.savefig('./plots/pred.pdf')
+    plt.scatter(x,y,c=mixed_colors)
+    plt.savefig('./plots/mixed.pdf')
