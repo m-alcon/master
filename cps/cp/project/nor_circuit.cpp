8d3fe0183f1c50b9c21a60ce6b368357df6fa8c0
miquelalcon <miquel.1996@gmail.com>
2019-04-29 16:52:03 +0200

    cps more idea

--- cps/cp/project/nor_circuit.cpp
+++ cps/cp/project/nor_circuit.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <fstream>
 
-#define MAX_DEPTH 1
+#define MAX_DEPTH 2
 
 using namespace std;
 using namespace Gecode;
@@ -17,7 +17,7 @@ protected:
   IntVar size;
   int n, depth;
 public:
-  NOR_Circuit(vector<int> t, int d) : n(t.size()), truth_table(t), depth(d), circuit(*this, pow(2,depth+1)-1, -2, n) {
+  NOR_Circuit(vector<int> t, int d) : n(t.size()), truth_table(t), depth(d), circuit(*this, pow(2,d+1)-1, -1, t.size()) {
     // Not NOR
     // for (int j = 0; j < N; ++j) {
     //   BoolVarArgs v(N);
@@ -31,8 +31,7 @@ public:
 
   NOR_Circuit(NOR_Circuit& c) : Space(c) {
     truth_table = c.truth_table;
-    for (int i = 0; i <= n; ++i)
-      circuit[i].update(*this, c.circuit[i]);
+    circuit.update(*this, c.circuit);
   }
 
   virtual Space* copy() {
@@ -45,19 +44,32 @@ public:
     return circuit[(pow(2,row)-1)+col];
   }
 
-  void analyze(const int & row, const int & col) {
-    if (row+1 > depth) {
+  int node_pos(const int & row, const int & col) {
+    if (col >= pow(2,row))
+      cout << "ERR: Trying to access an invalid node." << endl;
+    return (pow(2,row)-1)+col;
+  }
+
+  bool is_bit_up(const unsigned int &bit_pos, const int &number) {
+    return (bit_pos & (unsigned int) pow(2,number)) != 0;
+  }
+
+  void relate_variables(const int & row, const int & col, const int &truth_idx, BoolVarArray & circuit_values) {
+    IntVar root = node(row,col);
+    if (row == depth) {
+      for (int i = 1; i < n; ++i)
+        rel(*this, (root == i) >> (circuit_values[node_pos(row,col)] == is_bit_up(n-1-i,truth_idx)));
     }
     else {
-      node(row,col);
-      analyze(row+1,2*col);
-      analyze(row+1,2*col+1);
+      for (int i = 1; i < n; ++i)
+        rel(*this, (root == i) >> (circuit_values[node_pos(row,col)] == is_bit_up(n-1-i,truth_idx)));
+      relate_variables(row+1,2*col, truth_idx, circuit_values);
+      relate_variables(row+1,2*col+1, truth_idx, circuit_values);
     }
-    
   }
 
   void print() const {
-    cout << "Need implementation." << endl;
+    //cout << "Need implementation." << endl;
     cout << circuit << endl;
   }
 
@@ -79,14 +91,15 @@ int main(int argc, char* argv[]) {
     return -1;
   }
   vector<int> truth_table = read_input(argv[1]);
-  for (int depth = 1; depth <= MAX_DEPTH; ++depth) {
+  for (int depth = 0; depth <= MAX_DEPTH; ++depth) {
     cout << "Solving with depth = " << depth << "." << endl;
     NOR_Circuit* c = new NOR_Circuit(truth_table,depth);
-    //DFS<NOR_Circuit> e(c);
-    //delete c;
-    //if (NOR_Circuit* s = e.next()) {
-    //  s->print(); delete s;
-    //} 
+    DFS<NOR_Circuit> e(c);
+    delete c;
+    if (NOR_Circuit* s = e.next()) {
+      s->print(); delete s;
+    }
+    cout << endl;
   }
-  cout << endl << "WAR: Depth should start at 0, now starts at 1." << endl;
+  cout << endl << "WAR: Depth should start at 0, now starts at 2." << endl;
 }
