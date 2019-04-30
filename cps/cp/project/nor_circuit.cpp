737060b55f7feebc8b3a630db06dcbc3465aa4e7
miquelalcon <miquel.1996@gmail.com>
2019-04-30 15:04:40 +0200

    cps project 1 sounds good

--- cps/cp/project/nor_circuit.cpp
+++ cps/cp/project/nor_circuit.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <fstream>
 
-#define MAX_DEPTH 2
+#define MAX_DEPTH 10
 
 using namespace std;
 using namespace Gecode;
@@ -14,32 +14,22 @@ class NOR_Circuit : public Space {
 protected:
   vector<int> truth_table;
   IntVarArray circuit;
-  IntVar size;
   int n, depth;
 public:
-  NOR_Circuit(vector<int> t, int d) : n(t.size()), truth_table(t), depth(d), circuit(*this, pow(2,d+1)-1, -1, t.size()) {
-    
-    for (int truth_idx = 0; truth_idx < n; ++truth_idx) {
-      BoolVarArgs circuit_vals(pow(2,d+1)-1);
-      // Relate variables
-      // for (int i = 0; i < n; ++i)
-      //   for (int j = 1; j <= n; ++j)
-      //       rel(*this, (circuit[i] == j) >> (circuit_vals[i] == is_bit_up(n-1-j,truth_idx)));
-      analyze(0, 0, truth_idx, circuit_vals);
+  NOR_Circuit(vector<int> t, int d) : n(log2(t.size())), truth_table(t), depth(d), circuit(*this, pow(2,d+1)-1, -1, log2(t.size())) {
+    for (int truth_idx = 0; truth_idx < t.size(); ++truth_idx) {
+      BoolVarArray circuit_bool(*this, pow(2,depth+1)-1, 0,1);
+      analyze(0, 0, truth_idx, circuit_bool);
+      rel(*this, circuit_bool[0] == ((bool)truth_table[truth_idx]));
     }
-    // Not NOR
-    // for (int j = 0; j < N; ++j) {
-    //   BoolVarArgs v(N);
-    //   for (int i = 0; i < N; ++i) 
-    //     v[i] = queen(i, j);
-    //   linear(*this, v, IRT_LQ, 1);
-    // }
-    //linear(*this, circuit, IRT_EQ, n); 
+
     branch(*this, circuit, INT_VAR_NONE(), INT_VAL_MIN()); // CAREFUL
   }
 
   NOR_Circuit(NOR_Circuit& c) : Space(c) {
     truth_table = c.truth_table;
+    n = c.n;
+    depth = c.depth;
     circuit.update(*this, c.circuit);
   }
 
@@ -47,72 +37,85 @@ public:
     return new NOR_Circuit(*this);
   }
 
-  IntVar node(const int & row, const int & col) {
+  int size () const {
+    int size = 0;
+    for (int i = 0; i < circuit.size(); ++i)
+      if (circuit[i].val() == -1)
+      	++size;
+    return size;
+  }
+
+  IntVar node(const int & row, const int & col) const {
     if (col >= pow(2,row))
-      cout << "ERR: Trying to access an invalid node." << endl;
+      cerr << "ERR: Trying to access an invalid node." << endl;
     return circuit[(pow(2,row)-1)+col];
   }
 
   int node_pos(const int & row, const int & col) {
     if (col >= pow(2,row))
-      cout << "ERR: Trying to access an invalid node." << endl;
+      cerr << "ERR: Trying to access an invalid node." << endl;
     return (pow(2,row)-1)+col;
   }
 
   bool is_bit_up(const unsigned int &bit_pos, const int &number) {
-    return (bit_pos & (unsigned int) pow(2,number)) != 0;
+    return ((unsigned int) pow(2, bit_pos) & number) != 0;
   }
 
-  void analyze(const int &row, const int &col, const int &truth_idx, BoolVarArgs &circuit_vals) {
-    cout << row << ", "<< col << endl;
+  void analyze(const int &row, const int &col, const int &truth_idx, BoolVarArray &circuit_bool) {
+    //cout << row << ", " << col << " = " << node_pos(row,col) << endl;
     IntVar root = node(row,col);
-    BoolVar root_val = circuit_vals[node_pos(row,col)];
+    BoolVar root_bool = circuit_bool[node_pos(row,col)];
     if (row == depth) {
-      rel(*this, (root == -1) >> (root_val == 0));
-      rel(*this, (root == 0) >> (root_val == 0));
-      for (int i = 1; i <= n; ++i)
-        rel(*this, (root == i) >> (root_val == is_bit_up(n-1-i,truth_idx)));
+      rel(*this, (root == -1) >> !root_bool);
+      rel(*this, (root == 0) >> !root_bool);
+      for (int i = 1; i <= n; ++i) {
+        rel(*this, (root == i) >> (root_bool == is_bit_up(n-i,truth_idx)));
+      }
     }
     else {
-      analyze(row+1, 2*col, truth_idx, circuit_vals);
-      analyze(row+1, 2*col+1, truth_idx, circuit_vals);
-      BoolVar left_val = circuit_vals[node_pos(row+1,2*col)];
-      BoolVar right_val = circuit_vals[node_pos(row+1, 2*col+1)];
-      rel(*this, (root == -1) >> (root_val == !(left_val || right_val)));
-      for (int i = 0; i <= n; ++i)
-        rel(*this, (root == i) >> (root_val == is_bit_up(n-1-i,truth_idx)));
+      analyze(row+1, 2*col, truth_idx, circuit_bool);
+      analyze(row+1, 2*col+1, truth_idx, circuit_bool);
+      BoolVar left_bool = circuit_bool[node_pos(row+1,2*col)];
+      BoolVar right_bool = circuit_bool[node_pos(row+1, 2*col+1)];
+      rel(*this, (root == -1) >> (root_bool == !(left_bool || right_bool)));
+      rel(*this, (root == 0) >> !root_bool);
+      for (int i = 1; i <= n; ++i) {
+        rel(*this, (root == i) >> (root_bool == is_bit_up(n-i,truth_idx)));
+      }
     }
   }
 
-  // void relate_vars(const int & row, const int & col, const int &truth_idx, BoolVarArray & circuit_vals) {
-  //   IntVar root = node(row,col);
-  //   if (row == depth) {
-  //     for (int i = 1; i < n; ++i)
-  //       rel(*this, (root == i) >> (circuit_vals[node_pos(row,col)] == is_bit_up(n-1-i,truth_idx)));
-  //   }
-  //   else {
-  //     for (int i = 1; i < n; ++i)
-  //       rel(*this, (root == i) >> (circuit_vals[node_pos(row,col)] == is_bit_up(n-1-i,truth_idx)));
-  //     relate_vars(row+1,2*col, truth_idx, circuit_vals);
-  //     relate_vars(row+1,2*col+1, truth_idx, circuit_vals);
-  //   }
-  // }
-
-  // SetExpr relate_variables(const int & row, const int & col, const int &truth_idx, BoolVarArray & circuit_values) {
-  //   IntVar root = node(row,col);
-  //   if (row == depth) { 
-  //     return singleton(root);
-  //   }
-  //   else {
-  //     SetExpr left = relate_variables(row+1,2*col, truth_idx, circuit_values);
-  //     SetExpr right = relate_variables(row+1,2*col+1, truth_idx, circuit_values);
-  //     return 1-max(left,right);
-  //   }
-  // }
+  void print_circuit(const int &row, const int &col, const int &id, int &remaining_id) const {
+    int code = node(row,col).val();
+    cout << id << " " << code;
+    if (code == -1) {
+      int left_id = remaining_id+1;
+      int right_id = remaining_id+2;
+      cout << " " << left_id << " " << right_id << endl;
+      remaining_id += 2;
+      print_circuit(row+1, 2*col, left_id, remaining_id);
+      print_circuit(row+1, 2*col+1, right_id, remaining_id);
+    }
+    else {
+      cout << " " << 0 << " " << 0 << endl;
+    }
+    
+  }
 
   void print() const {
-    //cout << "Need implementation." << endl;
-    cout << circuit << endl;
+    cout << n << endl;
+    for (int i = 0; i < truth_table.size(); ++i) {
+      cout << truth_table[i] << endl;
+    }
+    cout << depth << " " << size() << endl;
+    int remaining_id = 1;
+    print_circuit(0, 0, remaining_id, remaining_id);
+    cerr << circuit << endl;
+  }
+
+  virtual void constrain(const Space& _c) {
+    const NOR_Circuit& c = static_cast<const NOR_Circuit&>(_c);
+    count(*this, circuit, -1, IRT_LE, c.size());
   }
 
 };
@@ -132,16 +135,30 @@ int main(int argc, char* argv[]) {
     cout << argv[0] <<" FILE\n\tFILE:\tPath of the input file." << endl;
     return -1;
   }
-  vector<int> truth_table = read_input(argv[1]);
-  for (int depth = 0; depth <= MAX_DEPTH; ++depth) {
-    cout << "Solving with depth = " << depth << "." << endl;
-    NOR_Circuit* c = new NOR_Circuit(truth_table,depth);
-    DFS<NOR_Circuit> e(c);
-    delete c;
-    if (NOR_Circuit* s = e.next()) {
-      s->print(); delete s;
+  try {
+    vector<int> truth_table = read_input(argv[1]);
+    for (int depth = 0; depth <= MAX_DEPTH; ++depth) {
+      cerr << "Solving with depth = " << depth << "." << endl;
+      NOR_Circuit* mod = new NOR_Circuit(truth_table, depth);
+      BAB<NOR_Circuit> e(mod);
+      delete mod;
+      NOR_Circuit *s, *sant;
+      sant = e.next();
+      if (sant != NULL and (s = e.next())) {
+        while (s != NULL) {
+          delete sant; 
+          sant = s;
+          s = e.next();
+        }
+        sant->print();
+        delete sant;
+        break;
+      }
     }
-    cout << endl;
   }
-  cout << endl << "WAR: Depth should start at 0, now starts at 2." << endl;
+  catch (Exception e) {
+    cerr << "Gecode exception: " << e.what() << endl;
+    return 1;
+  }
+  return 0;
 }
