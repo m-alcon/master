a63453b1892e3cc3bcd905a8058e5ea39885de8d
miquelalcon <miquel.1996@gmail.com>
2019-06-12 12:58:02 +0200

    working kdtree

--- ads/final/src/kd_tree.cc
+++ ads/final/src/kd_tree.cc
@@ -1,6 +1,6 @@
 #include "kd_tree.h"
 
-void print_point(const Point &p) {
+void print_point (const Point &p) {
     cout << "(";
     cout << p[0];
     for (int j = 1; j < p.size(); ++j)
@@ -8,38 +8,141 @@ void print_point(const Point &p) {
     cout << ")" << endl;
 }
 
-void print_point_vector(const PointVector &v) {
+void print_point_vector (const PointVector &v) {
     for (int i = 0; i < v.size(); ++i) {
         print_point(v[i]);
     }
+    cout << endl;
 }
 
-Point KDTree::median(const uint &axis, PointVector v) {
+KDTree::KDTree (const uint &dim) {
+    this->dim = dim;
+}
+
+KDTree::KDTree (const uint &dim, const PointVector &v) {
+    this->dim = dim;
+    if (this->check_dimension(v)) {
+        this->build_standard(v);
+    }
+    else
+        throw string("Dimensions does not match.");
+    
+}
+
+bool KDTree::check_dimension(const PointVector &v) {
+    for (const Point element : v) {
+        if (element.size() != this->dim)
+            return false;
+    }
+    return true;
+}
+
+ostream & operator << (std::ostream & out, const KDTree & kdt) {
+    out << "( " << kdt.axis << " | " << kdt.location[0];
+    for (int i = 1; i < kdt.location.size(); ++i) {
+        out << "," << kdt.location[i];
+    }
+    out << " )";
+    out << kdt.left << kdt.right;
+    return out;
+}
+
+ostream & operator << (std::ostream & out, const KDTree* kdt) {
+    if (kdt != NULL) {
+        out << endl << "( " << kdt->axis ;
+        out << " | " << kdt->location[0];
+
+        for (int i = 1; i < kdt->location.size(); ++i) {
+            out << "," << kdt->location[i];
+        }
+        out << " )";
+        out << kdt->left << kdt->right;
+    }
+    else
+        out << "*";
+    return out;
+}
+
+Point KDTree::qselect (const uint &k, const uint &axis, PointVector v) {
+    Point pivot = v[rand() % v.size()];
+    PointVector left, right;
+    uint elements_like_pivot = 0;
+    for (Point element: v) {
+        if (element[axis] < pivot[axis])
+            left.push_back(element);
+        else if (element[axis] > pivot[axis])
+            right.push_back(element);
+        else
+            ++elements_like_pivot;
+    }
+    if (k < left.size())
+        return qselect(k, axis, left);
+    else if (k >= left.size() + elements_like_pivot)
+        return qselect(k-left.size()-elements_like_pivot, axis, right);
+    else
+        return pivot;
+}
+
+//Point KDTree::median(const uint &axis, PointVector v) {
+//    uint median_pos;
+//    if (v.size() % 2 == 0) 
+//        median_pos = v.size()/2;
+//    else
+//        median_pos = (v.size()-1)/2;
+//    auto comp = [&](Point x, Point y) {return x[axis] < y[axis];};
+//    sort(v.begin(), v.end(), comp);
+//    print_point_vector(v);
+//    return v[median_pos];
+//}
+
+Point KDTree::median (const uint &axis, PointVector v) {
     uint median_pos;
     if (v.size() % 2 == 0) 
         median_pos = v.size()/2;
     else
         median_pos = (v.size()-1)/2;
-    auto comp = [&](Point x, Point y) {return x[axis] < y[axis];};
-    sort(v.begin(), v.end(), comp);
-    print_point_vector(v);
-    return v[median_pos];
+    return KDTree::qselect(median_pos, axis, v);
 }
 
+void KDTree::build_standard (const PointVector &v) {
+    if (!v.empty()) {
+        this->dim = uint(dim);
+        this->axis = rand() % dim;
+        this->location = KDTree::median(this->axis,v);
+        PointVector left, right;
+        for (auto element: v) {
+            if (element != this->location) {
+                if (element[this->axis] <= this->location[this->axis])
+                    left.push_back(element);
+                else
+                    right.push_back(element);
+            }
+        }
+        if (!left.empty()) {
+            this->left = new KDTree(this->dim);
+            this->left->build_standard(left);
+        }
+        else
+            this->left = NULL;
 
-KDTree::KDTree (const uint &dim) {
-
-}
-
-KDTree::KDTree (const uint &dim, const PointVector &v) {
-    this->dim = dim;
-    print_point(KDTree::median(1,v));
-}
-
-KDTree* KDTree::build_default (const uint &i, const PointVector &v) {
-
+        if (!right.empty()) {
+            this->right = new KDTree(this->dim);
+            this->right->build_standard(right);
+        }
+        else
+            this->right = NULL;
+    }
+    else {
+        this->axis = 0;
+        this->location = Point (this->dim,0);
+        this->left = NULL;
+        this->right = NULL;
+    }
+    
 }
 
 int main() {
-    KDTree (0, {{5,2},{9,5},{2,6},{4,3},{3,4},{6,1}});
+    srand (time(NULL));
+    KDTree t (2, {{5,2},{9,5},{2,6},{4,3},{3,4},{6,1}});
+    cout << t << endl;
 }
--- ads/final/src/kd_tree.h
+++ ads/final/src/kd_tree.h
@@ -12,13 +12,19 @@ class KDTree {
     public:
         KDTree (const uint &dim);
         KDTree (const uint &dim, const PointVector &v);
-        KDTree* build_default (const uint &i, const PointVector &v);
+        friend ostream & operator << (std::ostream & out, const KDTree & kdt);
+        friend ostream & operator << (std::ostream & out, const KDTree* kdt);
 
-    private:
-        uint dim;
-        float location;
+        Point location;
         KDTree* left;
         KDTree* right;
+        uint dim, axis;
+
+    private:   
 
         static Point median(const uint &axis, PointVector v);
+        static Point qselect(const uint &k, const uint &axis, PointVector v);
+        //static KDTree* build_standard (const uint &dim, const PointVector &v);
+        void build_standard (const PointVector &v);
+        bool check_dimension(const PointVector &v);
 };
